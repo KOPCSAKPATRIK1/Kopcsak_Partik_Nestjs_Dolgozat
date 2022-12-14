69a13cb4cffaae5f17780a544296529beb968774
kopcsak patirk <kopcsakpatrik1@gmail.com>
2022-12-14 13:33:17 +0100

    kereses mukodik

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Query } from '@nestjs/common';
 import { Param } from '@nestjs/common';
+import { Post } from '@nestjs/common';
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
@@ -10,22 +11,22 @@ export class AppController {
 
   @Get()
   @Render('index')
-  async index() {
-    const [rows] = await db.execute(
-      'select * from macskak order by macskak.suly desc',
-    );
-    return {
-      macskak: rows,
-    };
-  }
-
-  @Get('index')
-  @Render('index')
-  async showPainting(@Query('szemszin') szemszin: string) {
-    const [rows] = await db.execute(
-      'select * from macskak where macskak.szem_szin like ?',
-      [szemszin],
-    );
-    return { macskak: rows };
+  async listMacskak(@Query('szemszin') szemszin) {
+    if (szemszin == null) {
+      const [rows] = await db.execute(
+        'select * from macskak order by macskak.suly desc',
+      );
+      return {
+        macskak: rows,
+      };
+    } else {
+      const [rows] = await db.execute(
+        'select * from macskak where macskak.szem_szin like ?',
+        [szemszin],
+      );
+      return {
+        macskak: rows,
+      };
+    }
   }
 }
